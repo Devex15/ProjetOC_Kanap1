e6ec60be7fdabe28368afc4cb2bde6c993297b96
xdang <xdangleterre@hotmail.com>
2024-06-27 11:39:03 +0200

    Fonction update qui va prendre en compte les modifs de quantité

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,7 +1,10 @@
 displayCommand(); 
 
+//======================================================================================
+// On crée une super fonction qui va créer les éléments du DOM de la commande client
+//=======================================================================================
+// Function displayCommand va chercher les infos du localStorage de l'Api et faire les affichages .
 
-// On crée une super fonction qui va créer les éléments du DOM
 function displayCommand() {
 
 // On crée un tableau cartClient qui va réunir les infos du local storage ( id color , qty)
@@ -17,11 +20,24 @@ if (cartClient.length === 0) {
 let cartCommand = [];
 
 // On fait appel à l'API en sélectionnant les objets du cartClient un par un . L'objectif étant d'afficher name descript venant de l'api :
-  cartClient.forEach((cartItem) => { 
+// On crée la variable fetchPromises qui va permettre d'attendre que toutes les promesses de fetch soient exécutées.
+
+let fetchPromises = cartClient.map((cartItem) => { 
   return fetch("http://localhost:3000/api/products/"+cartItem.id)
     .then((res) => res.json())
     .then((apiProduct) => {getInfoCartClient(apiProduct, cartItem);});
-});
+  });
+  
+  // ================================================================================================================================
+  // On utilise la Promise.all afin que toutes les promesses de réponse de l'API soient exécutées avant d'afficher  la commande client 
+  //==================================================================================================================================
+  // Sans elle , le carCommand s'affiche autant de fois qu'il y a de requête à l'API : Testé et a créé des problèmes d'affichage 
+  // Les fonction updateTotal() et displayCartItem() vont servir à afficher les prix et les éléments du DOM.
+
+  Promise.all(fetchPromises).then(() => {
+      updateTotals();
+      displayCartItem();
+      });
 
 
 //============================================================================================================
@@ -39,28 +55,24 @@ function getInfoCartClient(apiProduct, cartItem) {
     color: cartItem.color,
     qty: cartItem.qty,
     price: apiProduct.price
-  });
-
-  // Les fonction updateTotal() et displayCartItem() vont servir à afficher les prix et les éléments du DOM.
-  updateTotals();
-  displayCartItem();
-}
+      });
+  }
 // ============================================================================================================
 // On crée updateTotals   va calculer les quantité ainsi que le prix en utilisant les infos de cartCommand
 //============================================================================================================
 function updateTotals() {
   let totalQty = cartCommand.reduce((accumulator, cartCommand) => {
     return accumulator + cartCommand.qty;
-  }, 0);
+    }, 0);
 
-  document.getElementById("totalQuantity").innerText = totalQty;
+    document.getElementById("totalQuantity").innerText = totalQty;
 
-  let totalPrice = cartCommand.reduce((accumulators, cartCommand) => {
+    let totalPrice = cartCommand.reduce((accumulators, cartCommand) => {
     return accumulators + (cartCommand.qty * cartCommand.price);
-  }, 0);
+    }, 0);
 
-  document.getElementById("totalPrice").innerText = totalPrice;
-}
+    document.getElementById("totalPrice").innerText = totalPrice;
+  }
 
 //========================================================================================================
 // On crée la fonction displayCartItem , appelée avec fetch afin d'afficher les éléments du DOM
@@ -94,6 +106,58 @@ function displayCartItem() {
   }).join('');
 
   element.innerHTML += article;
+
+  // La fonction updateCommand  va être appelé afin de prendre en compte les modifications de quantité par le client. 
+  updateCommand();
 }
+
+}
+
+//========================================================================================================
+// On crée la fonction udateCommand qui va récupérer l'id , la couleur et la Qty et faire les modifications
+// =====================================================================================================
+// On utilisera location.reload() afin de valider les modifications .
+function updateCommand() {
+  // On recherche les sélecteurs qui ont comme class itemQuantity donc les input .
+  // On leur met un addEventListener qui va considérer les changements. 
+document.querySelectorAll('.itemQuantity').forEach((input) => {
+  input.addEventListener('change', function(event) {
+
+    // on recherche les sélecteur parent de itemQuantity et on se trouve les infos sur l'id et la color 
+    let cartItem = event.target.closest('.cart__item');
+
+    let updateId = cartItem.getAttribute('data-id');
+    let updateColor = cartItem.getAttribute('data-color');
+    let updateQty = event.target.value;
+    console.log('id', updateId); 
+    console.log('color:', updateColor);
+    console.log('Qty', updateQty);
+// On recharge le cart du local storage et on l'appelle cartUpdate
+  let cartUpdate = localStorage.getItem("cart") ? JSON.parse(localStorage.getItem("cart")) : [];
+
+
+// on recherceh l'index qui va être concerné par la modification. 
+  let indexUpdate = cartUpdate.findIndex((item) => item.id === updateId && item.color === updateColor);
+
+  // On effectue la modification
+  if (indexUpdate !== -1) {
+    cartUpdate[indexUpdate].qty = parseInt(updateQty);
+      }
+
+      console.log(cartUpdate);
+
+     localStorage.setItem('cart',JSON.stringify(cartUpdate)); 
+
+     displayCommand();
+
+     // On utilise location.reload() qui va recharger la page html et permettre la prise en compte des modifications. 
+     location.reload();
+    
+     });
+  });
+}
+
+function modifCommand() {
+
 
 }
